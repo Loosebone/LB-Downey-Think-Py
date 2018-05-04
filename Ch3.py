9a5975fd6091f0258d3ce388a126cea945352a7e
joelb <loosebone@github.com>
2018-05-04 14:45:07 +0000

    Fix for Python 3

--- Ch3.py
+++ Ch3.py
@@ -1,13 +1,16 @@
 # Attempts at exercises for Downey "Think Python" chapter 3
+# (now updated for Python 3)
+
 
 def right_justify(raw_str):
     pad = 70 - len(raw_str)
     if pad < 0:
         right_justify("Line too long")
     else:
-        print (' ' * pad) + raw_str
+        print((' ' * pad) + raw_str)
 
-print "Testing right_justify"
+
+print("Testing right_justify")
 right_justify("Hello")
 right_justify("Hello world - the quick brown fox jumps over the lazy dog")
 right_justify("Hello world!" * 8)
